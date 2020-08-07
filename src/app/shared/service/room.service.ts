c2d95630aa11bba4e4c0f4e28fdb8bba47c8c653
Your Name <you@example.com>
2020-08-07 04:01:26 +0300

    add service

---
+++ src/app/shared/service/room.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Room} from '../models/room';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+  private baseUrl = 'http://localhost:8080/rooms';
+  message: any;
+  constructor(private http: HttpClient) { }
+
+  getRoom(id: number): Observable<Object> {
+    return this.message = this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createRoom(room: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, room);
+  }
+
+  updateRoom(id: number, room: Room): Observable<Object> {
+    console.log(room);
+    room.id = null;
+    return this.http.put(`${this.baseUrl}/${id}`, room);
+  }
+
+  deleteRoom(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+  }
+
+  getRoomList(): Observable<any> {
+    return this.http.get(this.baseUrl);
+  }
+}
